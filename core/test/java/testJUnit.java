0d9cd10da42da108674e8f155d2fae10620f8e40
BrunoDavies <bd775@york.ac.uk>
2020-01-28 12:38:00 +0000

    added testing file and a test test (?)

---
+++ core/test/java/testJUnit.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class testJUnit {
+
+    @Test
+    public void testingJUnit() {
+        assertEquals(0, 0);
+    }
+}
