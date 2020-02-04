520c617db2a6396bfb6b03372c2e1678ae2e2f00
Lucy <li571@york.ac.uk>
2020-02-04 13:08:42 +0000

    Fixed menu button

--- core/src/com/dicycat/kroy/screens/GameOverScreen.java
+++ core/src/com/dicycat/kroy/screens/GameOverScreen.java
@@ -35,8 +35,8 @@ public class GameOverScreen implements Screen{
 	  private Texture youLostImage= new Texture("youlost.png");
 	  private Texture playButton= new Texture("newgame.png");
 	  private Texture playButtonActive= new Texture("newActive.png");
-	  private Texture menuButton = new Texture("menu.png");
-	  private Texture menuButtonActive = new Texture("menuActive.png");
+	  private Texture menuButton = new Texture("EXIT.png");
+	  private Texture menuButtonActive = new Texture("exitActive.png");
 	  
 	  private Integer score;
 	  private Integer highScore; 
