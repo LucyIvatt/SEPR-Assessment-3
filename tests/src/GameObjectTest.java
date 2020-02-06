fd05130de00f6cb4f987000fd3ae0625f069685f
BrunoDavies <bd775@york.ac.uk>
2020-02-06 19:54:47 +0000

    Full test coverage of GameObject init and .getCentre function

--- tests/src/GameObjectTest.java
+++ tests/src/GameObjectTest.java
@@ -13,22 +13,48 @@ import static org.junit.Assert.*;
 public class GameObjectTest {
 
     private GameObjectTester gameObjectTest;
+    private GameObjectTester gameObjectTestZeros;
 
     @Before
     public void init() {
+        //standard init of GameObject at (0,0) position, a image of a firetruck, with size (50,50)
         gameObjectTest = new GameObjectTester(new Vector2(0, 0),
                 new Texture("../core/assets/fireTruck1.png"), new Vector2(50, 50));
+
+        //No size init of GameObject at (50,50) position, a image of a firetruck, with size (0,0)
+        gameObjectTestZeros = new GameObjectTester(new Vector2(50, 50),
+                new Texture("../core/assets/fireTruck1.png"), new Vector2(0, 0));
     }
 
+    //Testing that the GameObject Constructor is working as intended with a standard input
     @Test
     public void initShouldInstantiateCorrectly() {
         assertFalse(gameObjectTest.isRemove());
     }
 
+    //Testing that when the GameObject is at location (0,0) .getCentre() calculates the correct centre
     @Test
-    public void initShouldSetCorrectDimensions() {
+    public void getCentreShouldReturnCentreWithZerosAsPosition() {
         assertEquals(new Vector2(25, 25), gameObjectTest.getCentre());
     }
+
+    //Testing that wen GameObject is at any 'middle' location .getCentre() calculates the correct centre
+    @Test
+    public void getCentreShouldReturnCentreAtStandardPosition() {
+        //setting position to simulate the wanted conditions
+        gameObjectTest.changePosition(new Vector2(50, 50));
+
+        assertEquals(new Vector2(75, 75), gameObjectTest.getCentre());
+    }
+
+    //Testing that when the dimensions are (0, 0) .getCentre() calculates the correct centre
+    @Test
+    public void getCentreShouldReturnCentreWithZeroSize() {
+        assertEquals(new Vector2(0, 0), gameObjectTestZeros.getCentre());
+    }
+
+
+
 }
 
 
