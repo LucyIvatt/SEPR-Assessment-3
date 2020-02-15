4d3af0cca2fccee56531272f6cf4d3e8d095dee0
Lucy Ivatt <li571@york.ac.uk>
2020-02-15 18:53:18 +0000

    added constants to button class

--- core/src/com/dicycat/kroy/misc/Button.java
+++ core/src/com/dicycat/kroy/misc/Button.java
@@ -19,24 +19,16 @@ public class Button {
 	private Texture button;
 	private Kroy game;
 	
-	private int buttonWidth = 250;
-	private int buttonHeight = 50;
-	private int xAxisCentred = (Kroy.width/2) - (buttonWidth/2);
-	
-	
-	/**
-	 * @param buttonY
-	 * @param button
-	 * @param buttonActive
-	 * @param game
-	 */
+	private final int BUTTON_WIDTH = 250;
+	private final int BUTTON_HEIGHT = 50;
+
+	private int xAxisCentred = (Kroy.width/2) - (BUTTON_WIDTH /2);
+
 	public Button(int buttonY, Texture button, Texture buttonActive, Kroy game) {
 		this.buttonY = buttonY;
 		this.button = button;
 		this.buttonActive = buttonActive;
 		this.game = game;
-		
-		
 	}
 	
 	/**
@@ -44,11 +36,10 @@ public class Button {
 	 * Also, if the mouse is on the button, the button changes to a different image of the button.
 	 */
 	public boolean buttonAction() {
-		
 		//if the mouse is on the button ...
-		if(( (Gdx.input.getX() < (xAxisCentred + buttonWidth)) && (Gdx.input.getX() > xAxisCentred) ) && ( (Kroy.height - Gdx.input.getY() > buttonY ) && (Kroy.height - Gdx.input.getY() < (buttonY + buttonHeight)) ) ){
+		if(( (Gdx.input.getX() < (xAxisCentred + BUTTON_WIDTH)) && (Gdx.input.getX() > xAxisCentred) ) && ( (Kroy.height - Gdx.input.getY() > buttonY ) && (Kroy.height - Gdx.input.getY() < (buttonY + BUTTON_HEIGHT)) ) ){
 			// ... then display the 'controlsButtonActice' texture ...
-			game.batch.draw(buttonActive, xAxisCentred, buttonY, buttonWidth, buttonHeight);
+			game.batch.draw(buttonActive, xAxisCentred, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT);
 			
 			  // if the controls button is pressed, show the visibility of the controls window to true and set the game state to CONTROLS
 			  if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
@@ -57,7 +48,7 @@ public class Button {
 			  }
 		  } else {
 			// ... otherwise, display the 'controlsButton' texture (as the mouse is neither clicking nor on the button, so display the default texture)
-			  game.batch.draw(button, xAxisCentred, buttonY, buttonWidth, buttonHeight);
+			  game.batch.draw(button, xAxisCentred, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT);
 		  }
 		
 		// nothing is happening with the button so just return false, no actions are being performed on it
